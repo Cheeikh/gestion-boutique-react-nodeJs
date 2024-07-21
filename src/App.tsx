9d6fec1c66720c1231a744a80bbfd5d35c0dafc0
Cheeikh <cheeikhmbacke.com>
2024-07-21 11:23:14 +0000

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import dataProvider, { GraphQLClient } from "@refinedev/graphql";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { authProvider } from "./authProvider";
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <DevtoolsProvider>
+          <Refine
+            dataProvider={gqlDataProvider}
+            routerProvider={routerBindings}
+            authProvider={authProvider}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              useNewQueryKeys: true,
+              projectId: "oCjyB2-7uvt1j-ic48hA",
+            }}
+          >
+            <Routes>
+              <Route index element={<WelcomePage />} />
+            </Routes>
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+          <DevtoolsPanel />
+        </DevtoolsProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
